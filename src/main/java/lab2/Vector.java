015cd66073662ff0b5f338a6aff45f8a8a012836
BioQwer <BioQwer94@gmail.com>
2014-07-06 13:33:26 +0400

    init

--- src/main/java/lab2/Vector.java
+++ src/main/java/lab2/Vector.java
@@ -6,20 +6,22 @@ public class Vector {
     private int[] array;
 
     public Vector(int vectorSize) {
-         setVectorSize(vectorSize);
+        setVectorSize(vectorSize);
     }
 
     public int getElement(int number) {
-        if(number<MIN_VECTOR_SIZE||number>=array.length)
+        if (number < MIN_VECTOR_SIZE || number >= array.length)
             return 0;
         else
             return array[number];
     }
 
-    public void setElement(int number,int element) {
-        if(number<MIN_VECTOR_SIZE||number>=array.length) {
-            array[number] = element;
+    public void setElement(int number, int element) {
+        if (number < MIN_VECTOR_SIZE || number >= array.length) {
+            array[number] = 0;
         }
+        else
+            array[number] = element;
     }
 
     public int getVectorSize() {
@@ -27,8 +29,25 @@ public class Vector {
     }
 
     public void setVectorSize(int vectorSize) {
-        if (vectorSize>0){
+        if (vectorSize > 0) {
             array = new int[vectorSize];
-        }
+        } else
+            array = new int[MIN_VECTOR_SIZE];
+    }
+
+    public int getMinElement(){
+        int min = array[0];
+        for (int i=0;i<array.length;i++)
+            if (array[i]<min)
+                min = array[i];
+        return min;
+    }
+
+    public int getMaxElement(){
+        int max = array[0];
+        for (int i=0;i<array.length;i++)
+            if (array[i]>max)
+                max  = array[i];
+        return max;
     }
 }
--- src/test/java/lab2/VectorTest.java
+++ src/test/java/lab2/VectorTest.java
@@ -1,18 +1,42 @@
 package lab2;
 
 import junit.framework.TestCase;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 public class VectorTest extends TestCase {
 
+    @Test
     public void testVectorConstructor() throws Exception {
+        System.out.println("VectorTest.testVectorConstructor");
         Vector vector;
-        for (int i=-10;i<-1;i++) {
+        for (int i=-10;i<-1;i++) {    //конструктор с отрицательной длиной
             vector = new Vector(i);
             assertEquals(0,vector.getVectorSize());
         }
+        for (int i=0;i<20;i++) {      //с положительной длиной
+            vector = new Vector(i);
+            assertEquals(i,vector.getVectorSize());
+        }
+    }
+
+    @Test
+    public void testMinMax() throws Exception {
+        System.out.println("VectorTest.testMinMax");
+        Vector vector = new Vector(20);
+        for (int i = 0; i<vector.getVectorSize();i++)
+            vector.setElement(i,i*100);
+        assertEquals(0,vector.getMinElement());
+        assertEquals(1900,vector.getMaxElement());
+    }
+
+    @Test
+    public void testSetGet() {
+        System.out.println("VectorTest.testSetGet");
+        Vector vector = new Vector(20);
+        for (int i = 0; i<vector.getVectorSize();i++)
+            vector.setElement(i,i);
+        for (int i = 0; i<vector.getVectorSize();i++)
+            assertEquals(i,vector.getElement(i));
     }
 }
