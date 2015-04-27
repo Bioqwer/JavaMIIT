734c31e4935e91166127bfb32c25858c60935be8
BioQwer <BioQwer94@gmail.com>
2015-04-27 09:52:18 +0300

    for lab3 simbols->digits

--- src/main/java/softwareSecurity/Lab2.java
+++ src/main/java/softwareSecurity/Lab2.java
@@ -36,7 +36,7 @@ public class Lab2 {
         return true;
     }
 
-    public static boolean simple(BigInteger p) {
+    public static boolean isPrimeWithLittle(BigInteger p) {
         int[] primesTo100 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
         //step 2  Compare with little primes
         for (int elements : primesTo100) {
@@ -102,7 +102,7 @@ public class Lab2 {
         while (!found) {
             p = generateFrom(q);
             // step 2 check for Prime with 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
-            while (!simple(p)) {
+            while (!isPrimeWithLittle(p)) {
                 p = generateFrom(q);
             }
             // Rabin miller test
--- src/test/java/softwareSecurity/Lab2Test.java
+++ src/test/java/softwareSecurity/Lab2Test.java
@@ -3,6 +3,7 @@ package softwareSecurity;
 import junit.framework.TestCase;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -18,5 +19,11 @@ public class Lab2Test extends TestCase {
         BigInteger p = new BigInteger(q.bitLength(), 0, ThreadLocalRandom.current());
         System.out.println("p = " + p);
         System.out.println(q4add2.compareTo(p));
+
+        String asd = "AQWERTYUIOPASDFGHJKLZXCVBNMZZ";
+        System.out.println("asd.getBytes() = " + Arrays.toString(asd.getBytes()));
+        String lower = "az";
+        lower = lower.toUpperCase();
+        System.out.println("lower.getBytes() = " + Arrays.toString(lower.getBytes()));
     }
 }
