079f264cf6ef9b5de3f6bbfb7ab8718fd55a03f3
Antony Alexsandrov <af.alexsandrov@gmail.com>
2017-02-05 12:56:12 +0300

    update travis

--- src/test/java/javaPreCompile/JsComputeTest.java
+++ src/test/java/javaPreCompile/JsComputeTest.java
@@ -8,14 +8,16 @@ import preCompile.JsCompute;
 
 public class JsComputeTest {
 
+  private final JsCompute jsCompute = new JsCompute();
+
   @Test
   public void evaluate() throws Exception {
-    JsCompute jsCompute = new JsCompute();
     Map<String, String> map = new HashMap<>();
     map.put("a", "2");
     map.put("b", "3");
     map.put("e", "10");
     map.put("d", "10");
+
     Number evaluate = jsCompute.evaluate(map, "c = a + b^2", "c");
     Assert.assertEquals(7, evaluate.intValue());
   }
